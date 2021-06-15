a099d788b5d451677df8e7fbd0c9d7eb8e9b17c0
rriyaldhi <rriyaldhi@gmail.com>
2021-06-15 10:29:33 +0200

    test

--- detect.py
+++ detect.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
 
     # testing begin
     total = 0
-    n = 1001
+    n = 25
     for i in range(n):
         image_path = "./images/2.4k.jpeg"
         img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
