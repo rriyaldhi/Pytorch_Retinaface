33fa4f0f51e8e10804992e4451ca0d3aad7298e5
rriyaldhi <rriyaldhi@gmail.com>
2021-06-15 10:09:50 +0200

    test

--- detect.py
+++ detect.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
     total = 0
     n = 26
     for i in range(n):
-        image_path = "./images/1.fullhd.jpeg"
+        image_path = "./images/1.4k.jpeg"
         img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
         img = np.float32(img_raw)
@@ -174,3 +174,4 @@ if __name__ == '__main__':
             name = "test.jpg"
             cv2.imwrite(name, img_raw)
 
+    print(total / (n - 1))
